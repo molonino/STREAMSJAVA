9e4c2266b667f56a92aa98ecade9651aa55269e6
molonino <44877803+molonino@users.noreply.github.com>
2019-03-28 18:05:46 +0100

    Add files via upload

---
+++ EscribeFichero.java
@@ -0,0 +1,14 @@
+/*
+Crear los metodos necesarios para escribir en un fichero, con un flujo de bytes 
+y como un flujo de caracteres aparte de utilizar un metodo con buffer
+ */
+package escrituralecturaficheros;
+
+/**
+ *
+ * @author Angel Losada Ramos
+ */
+public class EscribeFichero {
+    
+
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+Escribir un programa que realice la lectura de un fichero txt y despues lo 
+escriba en otro fichero txt con un formato que sea facil de interpretar
+ */
+package escrituralecturaficheros;
+
+import java.io.*;
+
+/**
+ * @author Angel Losada Ramos
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+         System.out.println(LeeFichero.mostrarArchivo());
+    }
+
+        
+}
+    
+
